30f340574caa11a3f92ff362b9fb97859a386a85
sreedhar.kumar <sreedhar.kumar@imtek.de>
2013-08-29 18:17:39 +0200

    updated 29.08.2013
    Signed-off-by: sreedhar.kumar <sreedhar.kumar@imtek.de>

--- theNewState.m
+++ theNewState.m
@@ -16,25 +16,26 @@ spks_samples = cleanspikes(spikes_samples);
 %% Generate `states'
 inAChannel = cell(60,1);
 binWidth = 10; %in ms
-states = zeros(10,ceil(max(spks_samples.time)/(25*binWidth))); % each states is now a column; 250 samples correspond to 10ms
+nBits = 10;
+states = zeros(nBits,ceil(max(spks_samples.time)/(25*binWidth))); % each states is now a column; 250 samples correspond to 10ms
 for ii=0:59
     inAChannel{ii+1,1} = spks_samples.time(spks_samples.channel==ii);
 end
 nSpikesInEachChannel = cellfun(@length,inAChannel);
 [sortedNSpikes, sortedIndx] = sort(nSpikesInEachChannel,'descend');
-nBits = 10;
+
 for ii = 1:nBits
 states(ii,ceil(inAChannel{sortedIndx(ii)}/(25*binWidth))) = 1;
 end
 states_str = num2str(states'); %each state is now a row
 states_dec = bin2dec(states_str); % each state is now an integer
 nullstateidx = find(states_dec ==0);
-nulltemp = [nullstateidx(1); nullstateidx(find(diff(nullstateidx)>1)+1)];
+nulltemp = [nullstateidx(1); nullstateidx(find(diff(nullstateidx)>1)+1)];  
 for ii = 1: length(nulltemp)
     nullstateidx(nullstateidx==nulltemp(ii)) = [];
 end
 red_states_dec = states_dec;
-red_states_dec(nullstateidx) = [];
+red_states_dec(nullstateidx) = []; % contiguous all zero states are clumped together as single states
 
 [b, m, n] = unique(red_states_dec);
 count = zeros(size(b));
@@ -62,13 +63,13 @@ ylabel('Probability','FontSize',12)
 % title(datRoot,'FontSize',12, 'Interpreter','none')
 
 %%
-vec = [1 2 4 8 16 32 64 128 256 512];
-fh4 = figure;
-for ii = 1:length(vec)
-    plot(ii,count_n(b==vec(ii)),'*-')
-    hold on
-end
-plot([1:10], 0.1*ones(1,10)); hold off
+% vec = [1 2 4 8 16 32 64 128 256 512];
+% fh4 = figure;
+% for ii = 1:length(vec)
+%     plot(ii,count_n(b==vec(ii)),'*-')
+%     hold on
+% end
+% plot([1:10], 0.1*ones(1,10)); hold off
 
 %% the probablities given a uniform dist.
 % fh5 = figure;
@@ -79,7 +80,7 @@ end
 fh6 = figure;
 semilogy([0:10],prob,'*-');
 %% 
-% leaving out zero
+
 values_by_nOnes = cell(nBits+1,1); % zero bits to nBits bits
 for ii = 0:2^nBits-1
 binary_rep = dec2bin(ii);
@@ -98,22 +99,18 @@ for ii = 1:nBits+1
 end
 
 fh7 = figure;
-for ii = 2:5%1:nBits+1
-    subplot(2,2,ii-1)
-    plot(values_by_nOnes{ii}(2,:),'--.','markersize',5);
+for ii = 1:nBits+1
+    subplot(3,4,ii)
+    plot(values_by_nOnes{ii}(2,:)/sum(values_by_nOnes{ii}(2,:)),'--.','markersize',5);
     hold on;
     xlim([0 size(values_by_nOnes{ii},2)]);
     xax = get(gca,'XLim');
     %(1:size(values_by_nOnes{ii},2))
-    plot(linspace(xax(1),xax(2),size(values_by_nOnes{ii},2)),prob(ii)*ones(1,size(values_by_nOnes{ii},2)),'r');
+    %plot(linspace(xax(1),xax(2),size(values_by_nOnes{ii},2)),prob(ii)*ones(1,size(values_by_nOnes{ii},2)),'r');
     title(['Class:',num2str(ii-1)],'FontSize',12);
     %axis tight
 end
-[ax1,h1] = suplabel('Various possible words in each case arranged in ascending order');
-[ax2,h2] = suplabel('Probability','y');
-set(h1,'FontSize',12);
-set(h2,'FontSize',12);
-tightfig;
+
 %%
 % fr profile of a single channel.
 % [nSpksPerBin,timeVec] = hist(spks_samples_51,0:25e3:max(spks_samples_51));
@@ -127,5 +124,22 @@ tightfig;
 %skewing the uniform probabilities
 
 nSpikesInChosen = sum(nSpikesInEachChannel(sortedIndx(1:nBits)));
-weights2skew = nSpikesInEachChannel(sortedIndx(1:nBits))/nSpikesInChosen;
+weights2skew = nSpikesInEachChannel(sortedIndx(1:nBits))/nSpikesInChosen; %from MSB to LSB
 
+set(0,'CurrentFigure',fh7);
+for ii = 1:nBits+1
+    bin_words = dec2bin(values_by_nOnes{ii}(1,:)); %list of binary words in each class
+    pseudoExpectation = zeros(size(bin_words,1),1);
+    for jj = 1:size(bin_words,1)
+        ones_pos = find(bin_words(jj,:) == '1');
+        pseudoExpectation(jj,1) = prod(weights2skew(ones_pos));
+    end
+    subplot(3,4,ii)
+    plot(pseudoExpectation,'--.k');
+end
+
+[ax1,h1] = suplabel('Various possible words in each case arranged in ascending order');
+[ax2,h2] = suplabel('Probability','y');
+set(h1,'FontSize',12);
+set(h2,'FontSize',12);
+tightfig;
