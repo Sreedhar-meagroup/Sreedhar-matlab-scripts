1c96771b27548d0a94ba358c9f54766837f086b7
sreedhar-kumar <sreedhar.kumar@imtek.de>
2014-04-10 10:39:42 +0200

    Signed-off-by: sreedhar-kumar <sreedhar.kumar@imtek.de>

--- extract_thresh.m
+++ extract_thresh.m
@@ -1,4 +1,7 @@
 function [thresh] = extract_thresh(filepath)
+% extract_thresh('filename') extracts the spike detection threshold from
+% the .desc file accompanying the data (see MeaBench recording format).
+
     str = fileread(filepath);
     expr = '\s*Threshold:\s*.*?\s*\d+\s*';
     fstr = regexp(str,expr,'match');
