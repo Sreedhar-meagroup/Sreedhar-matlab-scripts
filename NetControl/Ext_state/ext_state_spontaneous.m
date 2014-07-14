a1149c919909ed1a83129f20a1bd0f644faad21b
sreedhar.kumar <sreedhar.kumar@imtek.de>
2014-07-14 18:01:02 +0200

    Signed-off-by: sreedhar.kumar <sreedhar.kumar@imtek.de>

---
+++ NetControl/Ext_state/ext_state_spontaneous.m
@@ -0,0 +1,15 @@
+spont_data = spontaneousData;
+
+%took 4569
+%take channel 57 (1-60) hw+1
+
+SpikeTimes = spont_data.InAChannel{57}; %hw+1
+Steps = 10.^[-5:.05:1.5];
+N = 2;
+valleyMinimizer_ms = HistogramISIn(SpikeTimes, N, Steps)
+Spike.T = SpikeTimes;
+Spike.C = 57*ones(size(Spike.T));
+[Burst SpikeBurstNumber] = BurstDetectISIn( Spike, 3, valleyMinimizer_ms/1e3 );
+
+burstDuration_s = Burst.T_end - Burst.T_start;
+preBSil_s = Burst.T_start(2:end) - Burst.T_end(1:end-1);
