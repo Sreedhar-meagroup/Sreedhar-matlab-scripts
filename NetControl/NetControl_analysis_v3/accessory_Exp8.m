ad88785d190bf3ef025bbae7b0154a582d5fb1bd
sreedhar.kumar <sreedhar.kumar@imtek.de>
2014-09-04 23:13:46 +0200

    Signed-off-by: sreedhar.kumar <sreedhar.kumar@imtek.de>

--- NetControl/NetControl_analysis_v3/accessory_Exp8.m
+++ NetControl/NetControl_analysis_v3/accessory_Exp8.m
@@ -1,5 +1,5 @@
 %% participation in spontaneous bursts
-recCh = 58;
+recCh = 51; %hw+1
 SpikeTimes = data.InAChannel{recCh}; %hw+1
 Steps = 10.^[-5:.05:1.5];
 N = 2;
@@ -7,13 +7,15 @@ valleyMinimizer_ms = HistogramISIn(SpikeTimes, N, Steps)
 Spike.T = data.InAChannel{recCh};
 Spike.C = recCh*ones(size(Spike.T));
 [Burst, BNum] = BurstDetectISIn(Spike, 3, 1);
-mean(Burst.S)
-std(Burst.S)
+disp(['Mean participation in SB: ', num2str(mean(Burst.S))]);
+disp(['Std. dev of participation in SB: ', num2str(std(Burst.S))]);
 
 
 %% response to stimuli
-chosen_stimInd = 3;
+chosen_stimInd = 2;
 % recCh = 17; %hw+1
 for ii = 1:50
     resp_length(ii) = length(find(resp_slices{chosen_stimInd}{ii}.channel == recCh-1));
 end
+disp(['Mean reponse length: ', num2str(mean(resp_length))]);
+disp(['Std. dev of response length: ', num2str(std(resp_length))]);
